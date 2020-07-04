ea83f81e7fffbef4e6252e87fd825e1a2d4c6fbe
Abdullatifalnajjar-372 <66475069+Abdullatifalnajjar-372@users.noreply.github.com>
2020-07-04 18:00:56 +0300

    cw

---
+++ pokemon/app/src/main/java/com/example/pokemon/pokemon.java
@@ -0,0 +1,24 @@
+package com.example.pokemon;
+
+public class pokemon {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int image ;
+
+    public pokemon() {
+    }
+
+    private int attack ;
+    private int defence ;
+    private int total ;
+
+
+}
